875c2e1076fdc2d7dd0c93f50e6a617accea7d7b
Aurooba Ahmed <hey@aurooba.com>
2021-02-03 08:16:05 -0700

    lesson 2 complete

--- functions.php
+++ functions.php
@@ -1,18 +1,21 @@
 <?php
 
 /*
- * Enqueue the parent stylesheet from Twenty Twenty One
+ * Child theme scripts
  */
-function enqueue_parent_styles() {
+function twenty_twenty_one_child_scripts() {
 	wp_enqueue_style(
 		'child-style',
 		get_stylesheet_uri(),
 		array( 'twenty-twenty-one-style' ),
 		wp_get_theme()->get( 'Version' ) // this only works if you have Version in the style header
 	);
+
+	//remove default Block Editor styles
+	// wp_dequeue_style( 'wp-block-library' );
 }
 
-add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+add_action( 'wp_enqueue_scripts', 'twenty_twenty_one_child_scripts', 20 );
 
 /*
  * Child Theme default setup
@@ -22,6 +25,10 @@ function twenty_twenty_one_child_setup() {
 	add_theme_support( 'editor-styles' );
 
 	add_editor_style( 'editor.css' ); // the path to your editor.css file
+
+	// remove theme support for the wide and full  alignment options in the Block Editor
+	// remove_theme_support( 'align-wide' );
+
 }
 
 add_action( 'after_setup_theme', 'twenty_twenty_one_child_setup', 12 );

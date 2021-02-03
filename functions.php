6a90c94de8cd30b1bdb085bcd26c8354a77064fb
Aurooba Ahmed <hey@aurooba.com>
2021-02-02 17:06:26 -0700

    corrected parent stylesheet handle

--- functions.php
+++ functions.php
@@ -4,7 +4,12 @@
  * Enqueue the parent stylesheet from Twenty Twenty One
  */
 function enqueue_parent_styles() {
-	wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css', '', '1.0', 'all' );
+	wp_enqueue_style(
+		'child-style',
+		get_stylesheet_uri(),
+		array( 'twenty-twenty-one-style' ),
+		wp_get_theme()->get( 'Version' ) // this only works if you have Version in the style header
+	);
 }
 
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );

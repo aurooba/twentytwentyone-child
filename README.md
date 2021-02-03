b3f0725ab36686964e9a306df7c755804b7827b8
Aurooba Ahmed <hey@aurooba.com>
2021-02-02 17:08:00 -0700

    lesson 1 requirements

--- README.md
+++ README.md
@@ -3,5 +3,5 @@
 If you choose to use SCSS in this project, here's the helpful command to generate the new CSS file and sourcemap, while watching for new changes and giving you helpful error information:
 
 ```
-sass --style=compressed --error-css --watch --color  scss/style.scss style.css
+sass --style=compressed --error-css --watch --color  scss/style.scss:style.css scss/editor.scss:editor.css
 ```
--- functions.php
+++ functions.php
@@ -13,3 +13,15 @@ function enqueue_parent_styles() {
 }
 
 add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+
+/*
+ * Child Theme default setup
+ */
+function twenty_twenty_one_child_setup() {
+	// Add support for Editor Styles in the Block Editor
+	add_theme_support( 'editor-styles' );
+
+	add_editor_style( 'editor.css' ); // the path to your editor.css file
+}
+
+add_action( 'after_setup_theme', 'twenty_twenty_one_child_setup' );
